0d28d305452d9ed4e5aff05086a651a1b6f91826
Eben <me@ebenroux.co.za>
2016-01-04 15:12:55 +0200

    - v1.1.12

--- Shuttle.Core.MSBuild/Properties/AssemblyInfo.cs
+++ Shuttle.Core.MSBuild/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.1.11.0")]
+[assembly: AssemblyVersion("1.1.12.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.1.11")]
+[assembly: AssemblyInformationalVersion("1.1.12")]
 [assembly: ComVisible(false)]
