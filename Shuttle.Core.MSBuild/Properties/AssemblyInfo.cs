89527e9b205a1e50862192a130644904dc2fbd0f
Eben <me@ebenroux.co.za>
2016-01-04 12:35:22 +0200

    - v1.1.9

--- Shuttle.Core.MSBuild/Properties/AssemblyInfo.cs
+++ Shuttle.Core.MSBuild/Properties/AssemblyInfo.cs
@@ -2,33 +2,33 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 #if NET40FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.0")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.0")]
 #endif
 
 #if NET45FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.5")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.5")]
 #endif
 
 #if NET451FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.5.1")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.5.1")]
 #endif
 
 #if NET452FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.5.2")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.5.2")]
 #endif
 
 #if NET46FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.6")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.6")]
 #endif
 
 #if NET461FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Modules for .NET Framework 4.6.1")]
+[assembly: AssemblyTitle("Shuttle.Core.MSBuild for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.1.8.0")]
+[assembly: AssemblyVersion("1.1.9.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
-[assembly: AssemblyProduct("Shuttle.ESB")]
+[assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.1.8")]
+[assembly: AssemblyInformationalVersion("1.1.9")]
 [assembly: ComVisible(false)]
