f8bb0d1c9aaf4eb49b2f5547c511df8cd9d3e189
Eben <me@ebenroux.co.za>
2017-08-03 22:23:27 +0200

    - added assembly info / project templates

--- Shuttle.Core.MSBuild/Installation.cs
+++ Shuttle.Core.MSBuild/Installation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using EnvDTE;
 using EnvDTE80;
 
@@ -12,7 +13,74 @@ namespace Shuttle.Core.MSBuild
 		private readonly object _nugetPackage;
 		private readonly Project _vsProject;
 
-		public Installation(string installPath, string toolsPath, object nugetPackage, object vsProject)
+	    private const string AssemblyInfoTemplate =
+	        @"using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if NET45FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.5"")]
+#endif
+
+#if NET451FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.5.1"")]
+#endif
+
+#if NET452FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.5.2"")]
+#endif
+
+#if NET46FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.6"")]
+#endif
+
+#if NET461FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.6.1"")]
+#endif
+
+#if NET462FULL
+[assembly: AssemblyTitle(""{package-name} for .NET Framework 4.6.2"")]
+#endif
+
+[assembly: AssemblyVersion(""1.0.0.0"")]
+[assembly: AssemblyCopyright(""Copyright © Eben Roux {year}"")]
+[assembly: AssemblyProduct(""Sebata.Ems"")]
+[assembly: AssemblyCompany(""Sebata"")]
+[assembly: AssemblyConfiguration(""Release"")]
+[assembly: AssemblyInformationalVersion(""1.0.0"")]
+[assembly: ComVisible(false)]
+";
+
+	    private const string ProjectFileTermplate =
+	        @"  <PropertyGroup>
+    <Framework Condition="" '$(Framework)' == '' "">net45-full</Framework>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net45-full'"">
+    <TargetFrameworkVersion>v4.5</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET45FULL</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net451-full'"">
+    <TargetFrameworkVersion>v4.5.1</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET451FULL</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net452-full'"">
+    <TargetFrameworkVersion>v4.5.2</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET452FULL</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net46-full'"">
+    <TargetFrameworkVersion>v4.6</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET46FULL</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net461-full'"">
+    <TargetFrameworkVersion>v4.6.1</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET461FULL</DefineConstants>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Framework)' == 'net462-full'"">
+    <TargetFrameworkVersion>v4.6.2</TargetFrameworkVersion>
+    <DefineConstants>$(DefineConstants);NET462FULL</DefineConstants>
+  </PropertyGroup>
+";
+
+        public Installation(string installPath, string toolsPath, object nugetPackage, object vsProject)
 		{
 			_installPath = installPath;
 			_toolsPath = toolsPath;
@@ -42,7 +110,14 @@ namespace Shuttle.Core.MSBuild
 				throw new ApplicationException("Could not determine solution path.");
 			}
 
-			var buildFolder = Path.Combine(solutionFolder, ".build");
+		    var projectFolder = Path.GetDirectoryName(_vsProject.Properties.Item("FullPath").Value.ToString());
+
+		    if (string.IsNullOrEmpty(projectFolder))
+		    {
+		        throw new ApplicationException("Could not determine project path.");
+		    }
+
+            var buildFolder = Path.Combine(solutionFolder, ".build");
 
 			if (!Directory.Exists(buildFolder))
 			{
@@ -76,9 +151,81 @@ namespace Shuttle.Core.MSBuild
 			buildFolderProject.ProjectItems.AddFromFile(Path.Combine(buildFolder, "Shuttle.Core.MSBuild.targets"));
 			buildFolderProject.ProjectItems.AddFromFile(Path.Combine(buildFolder, "package.msbuild"));
 			buildFolderProject.ProjectItems.AddFromFile(Path.Combine(buildFolder, "package.nuspec"));
-		}
 
-		public void CopyBuildRelatedFile(string buildFolder, string fileName)
+		    _vsProject.Save();
+
+		    OverwriteAssemblyInfo(projectFolder);
+		    AddFramework(projectFolder);
+        }
+
+	    private void AddFramework(string projectFolder)
+	    {
+	        var projectFilePath = Path.Combine(projectFolder, _vsProject.FileName);
+
+	        if (!File.Exists(projectFilePath))
+	        {
+	            return;
+	        }
+
+	        try
+	        {
+	            var result = new StringBuilder();
+
+	            using (var sr = new StreamReader(projectFilePath))
+	            {
+	                string line;
+	                var added = false;
+
+	                while (!string.IsNullOrEmpty(line = sr.ReadLine()))
+	                {
+	                    if (!line.Contains("<TargetFrameworkVersion>"))
+	                    {
+	                        if (added)
+	                        {
+	                            result.AppendLine(line);
+	                        }
+	                        else
+	                        {
+	                            if (line.Contains("<ItemGroup>"))
+	                            {
+	                                result.Append(ProjectFileTermplate);
+
+	                                added = true;
+	                            }
+
+	                            result.AppendLine(line);
+	                        }
+	                    }
+	                }
+	            }
+
+	            File.WriteAllText(projectFilePath, result.ToString());
+	        }
+	        catch
+	        {
+	        }
+	    }
+
+	    private void OverwriteAssemblyInfo(string projectFolder)
+	    {
+	        var aseemblyInfoPath = Path.Combine(projectFolder, "Properties\\AssemblyInfo.cs");
+
+	        if (!File.Exists(aseemblyInfoPath))
+	        {
+	            return;
+	        }
+
+	        try
+	        {
+	            File.WriteAllText(aseemblyInfoPath,
+	                AssemblyInfoTemplate.Replace("{package-name}", _vsProject.Name).Replace("{year}", DateTime.Now.ToString("yyyy")));
+	        }
+	        catch
+	        {
+	        }
+	    }
+
+        public void CopyBuildRelatedFile(string buildFolder, string fileName)
 		{
 			var sourceFileName = Path.Combine(_toolsPath, fileName);
 			var targetFileName = Path.Combine(buildFolder, fileName);
